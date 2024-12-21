39cf077de963d77e558298762bbd14125d396e12
EJLECTRON <ish.myk0la@gmail.com>
2024-12-21 19:25:03 +0100

    Added connection to a test repo with test user

--- services/categoty_service.py
+++ services/categoty_service.py
@@ -1,4 +1,12 @@
+import os
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
 
+load_dotenv()
+
+SQL_ROOT_PASSWORD = os.getenv('SQL_ROOT_PASSWORD')
+
+engine = create_engine(f"mysql+mysqlconnector://mykola:{SQL_ROOT_PASSWORD}@localhost/EXPENSE_TRACKER?charset=utf8mb4&collation=utf8mb4_general_ci", echo=True)
 
 
 def add_category(name: str, datatype: str):
@@ -6,4 +14,8 @@ def add_category(name: str, datatype: str):
 
 
 def view_categories():
-    print("All categories listed here")
+    print("All categories listed here:")
+    with engine.connect() as connection:
+        output = connection.execute(text("SELECT NAME FROM CATEGORIES"))
+        for el in output:
+            print(el[0])
--- services/expence_service.py
+++ services/expence_service.py
@@ -1,8 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
+load_dotenv()
+
+SQL_ROOT_PASSWORD = os.getenv('SQL_ROOT_PASSWORD')
+
+engine = create_engine(f"mysql+mysqlconnector://mykola:{SQL_ROOT_PASSWORD}@localhost/EXPENSE_TRACKER?charset=utf8mb4&collation=utf8mb4_general_ci", echo=True)
 
 def add_expense(name: str, datatype: str, amount: float) -> str:
-    pass
+    with engine.connect() as connection:
+        query = f"INSERT INTO EXPENSES (A) VALUES ({name})"
+        connection.execute(query)
 
 
-def view_expenses(number=1, category=""):
-    pass
+def view_expenses(number=1, category="ENTERTAINMENT"):
+    with engine.connect() as connection:
+        query = f"SELECT * FROM EXPENSES WHERE CATEGORY = {category} LIMIT {number}"
+        connection.execute(query)
