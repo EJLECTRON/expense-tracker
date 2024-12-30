c05e3ba7e32be25b7c6f98f467d5391128a53d70
EJLECTRON <ish.myk0la@gmail.com>
2024-12-30 22:26:33 +0100

    Editing install scripts for database and user

--- install_scripts/set_up_user.sql
+++ install_scripts/set_up_user.sql
@@ -1,17 +1,25 @@
-DELIMITER $$
-CREATE PROCEDURE CreateUserIfNotExists(IN username VARCHAR(255))
+USE EXPENSE_TRACKER;
+
+DELIMITER &&
+CREATE PROCEDURE create_user(IN user_name VARCHAR(255), IN user_password VARCHAR(255))
 BEGIN
     DECLARE user_count INT;
 
     SELECT COUNT(*) INTO user_count
     FROM mysql.user
-    WHERE user = username AND host = 'localhost';
+    WHERE user=user_name AND host="localhost";
 
-    IF user_count = 0 THEN CREATE USER username@'localhost' IDENTIFIED BY password;
-END$$
+    IF user_count = 0 THEN
+        SET @query = CONCAT('CREATE USER \'', user_name, '\'@\'localhost\' IDENTIFIED BY \'', user_password, '\';');
+        PREPARE stmt FROM @query;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    END IF;
+END&&
+
 DELIMITER;
 
-CALL CreateUserIfNotExists('mykola', '#linux_password')
+CALL create_user('mykola', 'linuxpassword');
 
 GRANT ALL PRIVILEGES ON EXPENSE_TRACKER.* TO 'mykola'@'localhost';
 
