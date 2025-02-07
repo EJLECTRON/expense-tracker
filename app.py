92e10d00af544a7b2aebea37ec3c64e7e73547e9
EJLECTRON <ish.myk0la@gmail.com>
2025-02-07 17:19:20 +0100

    Refactored spacing

--- app.py
+++ app.py
@@ -5,29 +5,29 @@ from services.expense_service import add_expense, view_expenses
 def main():
     parser = argparse.ArgumentParser(description='Expense Tracker CLI')
     subparsers = parser.add_subparsers(dest='command')
-    
+
     view_categories_parser = subparsers.add_parser('view-categories')
 
     add_category_parser = subparsers.add_parser('add-category')
     add_category_parser.add_argument('name', type=str, help='Categoty name')
-    
+
     edit_category_parser = subparsers.add_parser('edit-category')
     edit_category_parser.add_argument('initial_name', type=str, help='Categoty name you want to rename')
     edit_category_parser.add_argument('needed_name', type=str, help='Desired new name of category')
-    
+
     delete_category_parser = subparsers.add_parser('delete-category')
     delete_category_parser.add_argument('name', type=str, help='Name of the category to delete')
-    
+
     add_expense_parser = subparsers.add_parser('add-expense')
     add_expense_parser.add_argument('name', type=str, help='Header of expence')
     add_expense_parser.add_argument('amount', type=float, help='Expense amount')
     add_expense_parser.add_argument('category', type=str, help='Category name')
     add_expense_parser.add_argument('description', type=str, help='Full description')
-    
+
     view_expenses_parser = subparsers.add_parser('view-expenses')
     view_expenses_parser.add_argument('number', type=int, help='View last n expenses')
     view_expenses_parser.add_argument('category', type=str, help='To discover all available categories use command: \n view categories')
-    
+
     args = parser.parse_args()
 
     if args.command == 'add-category':
@@ -42,7 +42,7 @@ def main():
         add_expense(args.name, args.amount, args.category, args.description)
     elif args.command == 'view-expenses':
         view_expenses(args.number, args.category)
-        
+
 
 if __name__ == '__main__':
     main()
