de932ac3b90de0e385847bc0b94339a0c72fd655
EJLECTRON <ish.myk0la@gmail.com>
2025-03-02 23:12:30 +0100

    Added delete expense function, fixed add expense and made some refactioring to key argument

--- expense_tracker/app.py
+++ expense_tracker/app.py
@@ -1,32 +1,48 @@
 import argparse
 from expense_tracker.services.category_service import add_category, view_categories, edit_category, delete_category
-from expense_tracker.services.expense_service import add_expense, view_expenses
+from expense_tracker.services.expense_service import add_expense, view_expenses, edit_expense, delete_expense
 
 def main():
     parser = argparse.ArgumentParser(description='Expense Tracker CLI')
     subparsers = parser.add_subparsers(dest='command')
 
+
+    help_main = subparsers.add_parser('--help')
+
     view_categories_parser = subparsers.add_parser('view-categories')
 
     add_category_parser = subparsers.add_parser('add-category')
-    add_category_parser.add_argument('name', type=str, help='Categoty name')
+    add_category_parser.add_argument('--name', type=str, required=True, help='Categoty name')
 
     edit_category_parser = subparsers.add_parser('edit-category')
-    edit_category_parser.add_argument('initial_name', type=str, help='Categoty name you want to rename')
-    edit_category_parser.add_argument('needed_name', type=str, help='Desired new name of category')
+    edit_category_parser.add_argument('--initial_name', type=str, required=True, help='Categoty name you want to rename')
+    edit_category_parser.add_argument('--needed_name', type=str, required=True, help='Desired new name of category')
 
     delete_category_parser = subparsers.add_parser('delete-category')
-    delete_category_parser.add_argument('name', type=str, help='Name of the category to delete')
+    delete_category_parser.add_argument('--name', type=str, required=True, help='Name of the category to delete')
 
     add_expense_parser = subparsers.add_parser('add-expense')
-    add_expense_parser.add_argument('name', type=str, help='Header of expence')
-    add_expense_parser.add_argument('amount', type=float, help='Expense amount')
-    add_expense_parser.add_argument('category', type=str, help='Category name')
-    add_expense_parser.add_argument('description', type=str, help='Full description')
+    add_expense_parser.add_argument('--name', type=str, required=True, help='Header of expence')
+    add_expense_parser.add_argument('--amount', type=float, required=True, help='Expense amount')
+    add_expense_parser.add_argument('--category', type=str, required=True, help='Category name')
+    add_expense_parser.add_argument('--description', type=str, required=False, help='Full description')
 
     view_expenses_parser = subparsers.add_parser('view-expenses')
     view_expenses_parser.add_argument('number', type=int, help='View last n expenses')
     view_expenses_parser.add_argument('category', type=str, help='To discover all available categories use command: \n view categories')
+
+
+    edit_expense_parser = subparsers.add_parser('edit-expense')
+
+
+    delete_expense_parser = subparsers.add_parser('delete-expense')
+    delete_expense_parser.add_argument("--all", action="store_true", required=False, help="Option to delete all occurencies with given options. It is should be used only if you want to delete all records of certain category, time, etc.")
+    delete_expense_parser.add_argument("--id", type=int, required=False, help="Id of the record you want to delete")
+    delete_expense_parser.add_argument("--name", type=str, required=False, help="Id of the record/s you want to delete")
+    delete_expense_parser.add_argument("--amount", type=float, required=False, help="Id of the record/s you want to delete")
+    delete_expense_parser.add_argument("--time", type=str, required=False, help="Id of the record/s you want to delete")
+    delete_expense_parser.add_argument("--category", type=str, required=False, help="Id of the record/s you want to delete")
+    delete_expense_parser.add_argument("--description", type=str, required=False, help="Id of the record/s you want to delete")
 
     args = parser.parse_args()
 
@@ -39,9 +55,13 @@ def main():
     elif args.command == 'edit-category':
         edit_category(args.initial_name, args.needed_name)
     elif args.command == 'add-expense':
-        add_expense(args.name, args.amount, args.category, args.description)
+        add_expense(name=args.name, amount=args.amount, category=args.category, description=args.description)
     elif args.command == 'view-expenses':
         view_expenses(args.number, args.category)
+    elif args.command == 'edit-expense':
+        edit_expense()
+    elif args.command == 'delete-expense':
+        delete_expense(id=args.id, name=args.name, amount=args.amount, time_of_transaction=args.time, category=args.category, description=args.description)
 
 
 if __name__ == '__main__':
--- expense_tracker/services/expense_service.py
+++ expense_tracker/services/expense_service.py
@@ -9,15 +9,66 @@ SQL_MYKOLA_PASSWORD = os.getenv('SQL_MYKOLA_PASSWORD')
 
 engine = create_engine(f'mysql+mysqlconnector://mykola:{SQL_MYKOLA_PASSWORD}@localhost/EXPENSE_TRACKER?charset=utf8mb4&collation=utf8mb4_general_ci', echo=True)
 
-def add_expense(name: str, amount: float, category: str, description: str) -> str:
+
+def add_expense(**kwargs):
+    """
+    Keyword arguments:
+        - name (str, required): Name of the record.
+        - amount (float, required): Amount of the record (pieces, grams, mililiters, etc.).
+        - category (str, required): category of the record from CATEGORIES table.
+        - description (str, optional): Description of the record. Defaults to ''.
+    """
+    category = kwargs.get("category").upper()
+    description = kwargs.get("description", "")
+
     with engine.connect() as connection:
-        query = text(f'INSERT INTO EXPENSES (TITLE, AMOUNT, TIME_OF_TRANSACTION, CATEGORY, DESCRIPTION) VALUES ("{name}", {amount}, "{datetime.now().strftime('%Y-%m-%d')}", "{category}", "{description}")')
+        query = text(f'INSERT INTO EXPENSES (TITLE, AMOUNT, TIME_OF_TRANSACTION, CATEGORY, DESCRIPTION) VALUES ' + \
+                    f'("{kwargs["name"]}", {kwargs["amount"]}, "{str(datetime.now().strftime('%Y-%m-%d'))}", ' + \
+                    f'"{category}", "{description}")')
         connection.execute(query)
         connection.commit()
 
 
-def view_expenses(number, category):
+def view_expenses(number: str, category:str):
+    category = category.upper()
     with engine.connect() as connection:
         query = text(f'SELECT * FROM EXPENSES WHERE CATEGORY=\'{category}\' LIMIT {number}')
         output = connection.execute(query)
         print(output.fetchall())
+
+
+def edit_expense():
+    pass
+
+
+def delete_expense(**kwargs):
+    """
+    Keyword arguments:
+        - id (int, optional): Id of the record (default. (default: 0; non-existent value in database).
+        - name (str, optional): Name of the record/s (default. (default: "")
+        - amount (float, optional): Amount of the record/s (default: 0.0)
+        - time_of_transaction (str, optional): Time of the record/s (default: "")
+        - category (str, optional): Category of the record/s (default: "")
+        - description (str, optional): Description of the record/s (default: "")
+    """
+    query, is_any_key_params = "DELETE FROM EXPENSES WHERE", False
+
+    for index, el in enumerate(kwargs):
+        if kwargs[el] != None: # checks if element was given by user
+            is_any_key_params = True
+            try:
+                query = query + f' {el}={kwargs[el]}' # adds key param to query
+                query += " AND"
+            except NameError:
+                pass
+
+    query = query[:len(query) - 3] + ';' # join semicolon to the end of query
+
+    if is_any_key_params:
+        with engine.connect() as connection:
+            query = text(query)
+            connection.execute(query)
+            connection.commit()
+        print("Deleted successfully.")
+    else:
+        print("None parameters were given, try again.")
