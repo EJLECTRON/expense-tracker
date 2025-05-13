a170bae5d6e7e136c83aea2241d5f81d3841901f
ejlectron <ish.myk0la@gmail.com>
2025-05-13 18:29:33 +0200

    Changed color of mesage for info messages

--- expense_tracker/services/logger.py
+++ expense_tracker/services/logger.py
@@ -3,18 +3,19 @@ import logging
 class CustomFormatter(logging.Formatter):
 
     grey = "\x1b[38;20m"
+    blue = "\x1b[34;20m"
     yellow = "\x1b[33;20m"
     red = "\x1b[31;20m"
     bold_red = "\x1b[31;1m"
     reset = "\x1b[0m"
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    time_format = "%(asctime)s - %(levelname)s: %(message)s (%(filename)s:%(lineno)d)"
 
     FORMATS = {
-        logging.DEBUG: grey + format + reset,
-        logging.INFO: grey + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset
+        logging.DEBUG: grey + time_format + reset,
+        logging.INFO: blue + time_format + reset,
+        logging.WARNING: yellow + time_format + reset,
+        logging.ERROR: red + time_format + reset,
+        logging.CRITICAL: bold_red + time_format + reset
     }
 
     def format(self, record):
